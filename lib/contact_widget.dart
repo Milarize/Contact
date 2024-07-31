a46f441c3f6361410d82cb368d81b6dac9fffb76
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-07-31 12:35:02 +0700

    Initial Project

---
+++ lib/contact_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContactWidget extends StatefulWidget {
+  const ContactWidget({ Key? key }) : super(key: key);
+
+  @override
+  _ContactWidgetState createState() => _ContactWidgetState();
+}
+
+class _ContactWidgetState extends State<ContactWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title :Text('My Contact'),),body :Center(child:Text('My Contact'))
+    );
+  }
+}
