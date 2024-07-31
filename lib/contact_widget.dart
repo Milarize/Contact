4539562f6b90e8923e813bb851df37d95e0d37f9
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-07-31 12:49:40 +0700

    ListView

--- lib/contact_widget.dart
+++ lib/contact_widget.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:my_contact/contact.dart';
 
 class ContactWidget extends StatefulWidget {
-  const ContactWidget({ Key? key }) : super(key: key);
+  const ContactWidget({Key? key}) : super(key: key);
 
   @override
   _ContactWidgetState createState() => _ContactWidgetState();
 }
 
 class _ContactWidgetState extends State<ContactWidget> {
+  List<Contact> contacts = [
+    Contact(id: 1, name: 'Test', phone: '0258741258', email: 'Test@gmail.com'),
+    Contact(id: 2, name: 'Test', phone: '0258741258', email: 'Test@gmail.com'),
+    Contact(id: 3, name: 'Test', phone: '0258741258', email: 'Test@gmail.com'),
+    Contact(id: 4, name: 'Test', phone: '0258741258', email: 'Test@gmail.com')
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title :Text('My Contact'),),body :Center(child:Text('My Contact'))
-    );
+        appBar: AppBar(
+          title: Text('My Contact'),
+        ),
+        body: ListView.builder(itemBuilder: (contact,index){
+          return ListTile(title:Text(contacts[index].name), );
+        },itemCount:contacts.length));
   }
 }
